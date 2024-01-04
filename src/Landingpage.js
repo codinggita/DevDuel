f12c356664db77ca99315c93103d4e1d53f923bd
Yash Patel <91671051+ydpatel0309@users.noreply.github.com>
2024-01-04 13:37:42 +0530

    enhanced teh quality of background  video and added social handels

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Landingpage.css";
 import { motion } from "framer-motion";
-import videoBg from "./images/fight.mp4";
-import logo from "./images/devduel.jpg";
-
+import videoBg from "./images/valorent fight.mp4";
+import githublogo from "./images/github-mark.svg";
+import linkdinlogo from "./images/linkdin.png";
 function LandingPg() {
   return (
     <>
@@ -12,12 +12,26 @@ function LandingPg() {
         <div className="content">
           <h1>DevDuel</h1>
           <h3>Programming quizz</h3>
+          <p> _____________________________</p>
 
-          <div class="button-borders">
-            <button class="primary-button">START</button>
+          <div className="btns">
+            <div>
+              <button class="btn"> Start now</button>
+              <div className="logos">
+                <img src={githublogo} alt="github logo" />
+              </div>
+            </div>
+            <div>
+              <button class="btn"> Explore now</button>
+              <div className="logos">
+                <img src={linkdinlogo} alt="linkidin logo" />
+              </div>
+            </div>
           </div>
         </div>
-        <video src={videoBg} autoPlay loop muted />
+        <div className="bgvideo-outer">
+          <video src={videoBg} autoPlay loop muted />
+        </div>
       </div>
     </>
   );
